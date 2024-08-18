3d1c8eb73af31da7114f3c9fe494b8e2ccfed5e7
Aditi-Wanjari06 <aditiWan2003@gmail.com>
2024-08-18 21:48:02 +0530

    clock UI created

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -6,6 +6,37 @@ function Home() {
   return (
     <div>
       <Navbar />
+
+      <div className='clock-container'>
+
+        <div className='clock-timer'>
+          <input
+            type='text'
+            className='clock-input'
+          />
+
+          <span className='clock-seperater'>:</span>
+
+          <input
+            type='text'
+            className='clock-input'
+          />
+
+          <span className='clock-unit'>hrs</span>
+        </div>
+
+        <div>
+          <p className='shift-timing'> Shift 9:00 AM - 5:00 PM </p>
+        </div>
+
+        <button type='button' className='btn-clock'>Clock in </button>
+
+      </div>
+
+
+
+
+
     </div>
   )
 }
